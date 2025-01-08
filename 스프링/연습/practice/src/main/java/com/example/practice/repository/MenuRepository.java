419143972a578dbf2d842a1a7d57cee810276747
skcy151515 <skcy151515@gmail.com>
2025-01-08 13:35:25 +0900

    폴더 변경

--- 스프링/연습/practice/src/main/java/com/example/practice/repository/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.practice.repository;
-
-import com.example.practice.model.MenuEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<MenuEntity, Long> {
-}
